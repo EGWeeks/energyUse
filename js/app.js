20b437a133708b8da7ec86410966ad9e92cd0747
Ethan Weeks <ethanweeks@ymail.com>
2015-11-20 15:17:14 -0700

    js getting form data

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
+	 if(document.location.search) {
+    var queryString = document.location.search.replace('?', '');
+    var pairs = queryString.split('&').map(function (pair) {
+      return values(pair.split('='));
+    });
+  }
+
+  function values(arr) {
+  	var formVal = [];
+  	formVal.push(arr[1]);
+  	return formVal;
+  }
 
 });
