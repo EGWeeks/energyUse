4ac0156bcaa339b5dc010fa0c32e4221c3a195a9
Ethan Weeks <ethanweeks@ymail.com>
2015-11-23 11:59:34 -0700

    hardcoded average $ per colo in graph

--- js/graph.js
+++ js/graph.js
@@ -1,47 +1,5 @@
 $(document).ready(function() {
-// Get context with jQuery - using jQuery's .get() method.
-var ctx = $("#myChart").get(0).getContext("2d");
-// This will get the first returned node in the jQuery collection.
-//
-// This chart needs to be generated AFTER THE DATA IS RETURNED!
-//
-var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July", "August"],
-    datasets: [
-        {
-            label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.2)",
-            strokeColor: "rgba(220,190,220,1)",
-            pointColor: "rgba(220,190,220,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(220,220,220,1)",
-            data: [65, 59, 80, 81, 56, 55, 40, 67]
-        },
-        {
-            label: "My Second dataset",
-            fillColor: "rgba(151,187,205,0.2)",
-            strokeColor: "rgba(151,187,255,1)",
-            pointColor: "rgba(151,187,235,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90, 45]
-        },
-        {
-            label: "My Second dataset",
-            fillColor: "rgba(151,167,205,0.2)",
-            strokeColor: "rgba(151,167,255,1)",
-            pointColor: "rgba(151,167,235,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [32, 67, 23, 89, 90, 50, 91, 56]
-        }
-    ]
-};
 
-var myLineChart = new Chart(ctx).Line(data);
 
 
 var apiKey = "5A3F06BA770E81ECBE0185475ED9F2B6";
@@ -55,7 +13,7 @@ $.ajax({
   }
 });
 
-
+// gets the last 8  month of electric cost and dates
 function responseElectricCost(data){
 	var monthlyElectricCost = [];
 	var months = [];
@@ -65,11 +23,57 @@ function responseElectricCost(data){
 		// array for dates
 		months.push(data.series[0].data[i][0]);
 	}
-	return calculateE(monthlyElectricCost, months);
+	return graphKWH(monthlyElectricCost, months);
 }
 
-function calculateE(costArr, monthArr) {
-	
+
+function userData() {
+	var userKWH = $('.kwh').text();
+}
+userData();
+
+
+// Graphing API calls 
+function graphKWH(costArr, timeArr) {
+	var coloradoEBill = {
+	jan : 91.10,
+	feb : 74.76,
+	march : 80.99,
+	april : 78.94,
+	may : 84.15,
+	june : 99.57,
+	july : 113.83,
+	august : 112.65,
+	sept : 76.02,
+	oct  : 69.41,
+	nov : 79.47,
+	dec : 92.73,
+};
+	console.log(costArr);
+	console.log(timeArr);
+	// Get context with jQuery - using jQuery's .get() method.
+var ctx = $("#myChart").get(0).getContext("2d");
+// This will get the first returned node in the jQuery collection.
+//
+// This chart needs to be generated AFTER THE DATA IS RETURNED!
+//
+var data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+    datasets: [
+        {
+            label: "Colorado average electric cost per kWh",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,190,220,1)",
+            pointColor: "rgba(220,190,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [coloradoEBill.jan, coloradoEBill.feb, coloradoEBill.march, coloradoEBill.april, coloradoEBill.may, coloradoEBill.june, coloradoEBill.july, coloradoEBill.august, coloradoEBill.sept, coloradoEBill.oct, coloradoEBill.nov, coloradoEBill.dec]
+        },
+    ]
+};
+
+var myLineChart = new Chart(ctx).Line(data);
 }
 
 });
